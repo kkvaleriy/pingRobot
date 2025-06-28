c0570a331d879c0dbf6bef0d569dc0e47a1b56ed
kkvaleriy <kvaleriy126@icloud.com>
2025-06-28 21:12:51 +0300

    feat: added set functional

--- internal/domain/services/services.go
+++ internal/domain/services/services.go
@@ -10,4 +10,19 @@ type services struct {
 	store map[string]service
 	mu    *sync.Mutex
 }
+
+func Set(serviceName, status string, timeOfCheck time.Time) {
+	once.Do(
+		func() {
+			s.store = make(map[string]service)
+			s.mu = &sync.Mutex{}
+		})
+
+	serv := service{
+		status:    status,
+		lastCheck: timeOfCheck,
+	}
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	s.store[serviceName] = serv
 }
