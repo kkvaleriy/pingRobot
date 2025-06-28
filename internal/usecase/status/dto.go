331e7b7a9bad2f6d6a4f09685ed3d07ef16290e6
kkvaleriy <kvaleriy126@icloud.com>
2025-06-28 21:52:27 +0300

    feat: added constructor for dto of status

--- internal/usecase/status/dto.go
+++ internal/usecase/status/dto.go
@@ -11,3 +11,17 @@ type StatusDto struct {
 	status    string
 	lastCheck time.Time
 }
+
+func createDto(s map[string]services.Service) []StatusDto {
+	statuses := []StatusDto{}
+
+	for name, service := range s {
+		statuses = append(statuses, StatusDto{
+			name:      name,
+			status:    service.Status(),
+			lastCheck: service.TimeOfLastCheck(),
+		})
+	}
+
+	return statuses
+}
