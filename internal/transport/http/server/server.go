a0deb86bb78ea8b0e07f46e9b8f83cc1e1f705a7
kkvaleriy <kvaleriy126@icloud.com>
2025-06-30 17:23:38 +0300

    feat: added handler for metrics

--- internal/transport/http/server/server.go
+++ internal/transport/http/server/server.go
@@ -15,6 +15,16 @@ var (
 	srv  *http.Server
 )
 
+func handleMetrics(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		metrics(w, r)
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+}
+
 func metrics(w http.ResponseWriter, r *http.Request) {
 	b, err := CreateJson(status.Check())
 	if err != nil {
